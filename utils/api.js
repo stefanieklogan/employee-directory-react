30916e8357858cb1deb63047671e0f8415880666
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-24 17:06:26 -0400

    update

--- utils/api.js
+++ utils/api.js
@@ -1,1 +1,9 @@
-import Axios from "axios"
+import axios from "axios";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  search: function(employees) {
+    return axios.get("https://randomuser.me/api/?inc=picture,name,email,phone,nat&results=10&nat=NZ"),
+    console.log(employees);
+  }
+};
