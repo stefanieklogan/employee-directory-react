9a154d94fe0471c38229724ad05ef17ab3f6a8dd
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-04-24 16:03:53 -0400

    tableHtml renders

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TableHtml from "./components/TableHtml";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Employee Directory
-        </p>
-      </header>
+      <TableHtml/>
     </div>
   );
 }
